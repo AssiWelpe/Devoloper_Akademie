cf9f9d0629b7a40d3b8935edef5f44ac2c7292ee
AssiWelpe <164692991+AssiWelpe@users.noreply.github.com>
2024-11-10 23:06:19 +0100

    Basics JS

---
+++ JS Grundlagen/dom.js
@@ -0,0 +1,7 @@
+let title = document.getElementById('website-title');
+    title.innerHTML = "Neuer Titel";
+    
+    
+    function toggleDnone(id){
+        document.getElementById('p_tag').classList.toggle('d_none');
+    }
---
+++ JS Grundlagen/object.js
@@ -0,0 +1,10 @@
+let myFancyObject = {
+    age : 32,
+    height : 178,
+    logname : function (){
+        console.log(123);
+        return 54323412;
+    }
+}
+
+console.log(myFancyObject.logname());
