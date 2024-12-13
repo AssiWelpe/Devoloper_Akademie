9db4cd4a8961c0afae2295f83f0251f227a077c0
AssiWelpe <164692991+AssiWelpe@users.noreply.github.com>
2024-12-13 19:23:33 +0100

    Delete Kochwelt directory

--- Kochwelt/js/script.js
+++
@@ -1,7 +0,0 @@
-function toggleMobileMenu(){
-    for (i of document.querySelectorAll(".mobile-button-group > img")) {
-        i.classList.toggle("mobile-hidden");
-    }
-    const nav = document.querySelector(".mobile-nav-links > nav");
-    nav.classList.toggle("nav-links-open");
-}
